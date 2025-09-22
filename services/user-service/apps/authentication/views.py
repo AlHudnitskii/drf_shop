1278cf8079dd3089d5cb3889d2ea2b98d2b4de74
alex <alhudnitskii@gmail.com>
2025-09-22 21:01:44 +0300

    end users in user-service

--- services/user-service/apps/authentication/views.py
+++ services/user-service/apps/authentication/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from apps.users.models import User
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_view(request):
+   email = request.data.get('email')
+   password = request.data.get('password')
+   
+   if not email or not password:
+      pass
+   
